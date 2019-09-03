88fee4640e8d40b16a0ff4a02992e6773e0297f2
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2019-09-03 10:13:07 +0200

    Variabler

--- LEK1.py
+++ LEK1.py
@@ -1,1 +1,10 @@
 print("hej på er i ee17")
+
+firstName = "Simon"
+lastName = "Östensson"
+dateOfBirth = 20010614
+age = 18
+teacher = False
+
+print(firstName, lastName + str(age) + str(teacher))
+print(dateOfBirth)
